021d3909b02bce877caa818c5bfed11bd805a271
mbs0221 <mbs2021@163.com>
2021-07-30 16:38:53 +0800

    add copy-pubkey

--- copy-pubkey.sh
+++ copy-pubkey.sh
@@ -12,7 +12,5 @@ dropbearkey -y -f $SSH_ROOT/dropbear_dss_host_key
 dropbearkey -y -f $SSH_ROOT/dropbear_ecdsa_host_key
 dropbearkey -y -f $SSH_ROOT/dropbear_ed25519_host_key
 
-# sudo ./copy-pubkey.sh /etc/dropbear | grep -E "ssh-rsa|ssh-dss|ecdsa-sha2-nistp256|ssh-ed25519" > authorized_keys
-# ./copy-pubkey.sh ~/.ssh/ | grep -E "ssh-rsa|ssh-dss|ecdsa-sha2-nistp256|ssh-ed25519" >> authorized_keys
-# cp authorized_keys root/.ssh/
-# cp authorized_keys home/ubuntu/.ssh/authorized_keys
+#./copy-pubkey.sh ~/.ssh/ | grep -E "ssh-rsa|ssh-dss|ecdsa-sha2-nistp256|ssh-ed25519" > /nfsroot/authorized_keys
+
