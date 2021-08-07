f25c07f24a98b8a880256526e1effb01c2f9fe15
mbs0221 <mbs2021@163.com>
2021-08-07 23:28:15 +0800

    add ftp-backup

---
+++ ftp-backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ftp -n<<!
+open vc709
+user ubuntu helloworld
+binary
+hash
+cd /nfsroot
+lcd /home/kiki212/rpmbuild/RPMS/x86_64
+prompt
+mkdir `date +"%Y%m%d"`
+cd `date +"%Y%m%d"`
+mput *
+close
+bye
+!
