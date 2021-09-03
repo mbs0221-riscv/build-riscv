132cea99650b0dafc4de3906a67db14289b149d7
mbs0221 <mbs2021@163.com>
2021-09-03 13:47:08 +0800

    complete version

--- write-boot.sh
+++ write-boot.sh
@@ -1,12 +1,8 @@
 #!/bin/bash -v
 
-CONFIG=$2
-
 dev=$1
-dtb=$CONFIG.dtb
+dtb=$2
 
-dtc -I dts -O dtb -o $CONFIG.dtb $CONFIG.dts
-
-dd if=./fw_jump.bin       of=$dev seek=0   bs=1M
-dd if=./Image             of=$dev seek=2   bs=1M
-dd if=./$dtb              of=$dev seek=29  bs=1M
+dd if=fw_jump.bin of=$dev seek=0
+dd if=Image       of=$dev seek=2   bs=1M
+dd if=$dtb        of=$dev seek=29  bs=1M
